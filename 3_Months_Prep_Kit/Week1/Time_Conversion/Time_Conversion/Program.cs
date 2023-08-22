b2bd3dd4a5fb300ee1ec85a0afb3bd70b33f051b
Togu <timeofgetup@gmail.com>
2023-08-21 21:06:22 -0500

    Time conversion solution

---
+++ 3_Months_Prep_Kit/Week1/Time_Conversion/Time_Conversion/Program.cs
@@ -0,0 +1,37 @@
+﻿static string timeConversion(string s)
+{
+    string hours = "";
+    int hoursInt = 0;
+    string minutes = "";
+    string seconds = "";
+    string meridiem = "";
+
+    hours = s.Substring(0, 2);
+    minutes = s.Substring(3, 2);
+    seconds = s.Substring(6, 2);
+    meridiem = s.Substring(8, 2);
+
+    hoursInt = Int16.Parse(hours);
+
+
+    if(meridiem.Equals("PM") && hoursInt != 12)
+    {
+        hoursInt += 12;
+        hours = hoursInt.ToString();
+    }
+
+    else if(meridiem.Equals("AM") && hoursInt == 12)
+    {
+        hours = "00";
+    }
+
+
+
+    return hours+":"+minutes+":"+seconds;
+
+
+}
+
+
+Console.WriteLine(timeConversion("07:05:45PM"));
+Console.WriteLine(timeConversion("12:45:54PM"));
