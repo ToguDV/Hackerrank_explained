bbe0ada2e208a8a0eefc6bdf931cf21354f17b50
Togu <timeofgetup@gmail.com>
2023-08-25 03:34:41 -0500

    Sparse Arrays solution

---
+++ 3_Months_Prep_Kit/Week1/Sparse_Arrays/Sparse_Arrays/Program.cs
@@ -0,0 +1,41 @@
+﻿
+static List<int> matchingStrings(List<string> strings, List<string> queries)
+{
+    List<int> resultsQuerys = new List<int>();
+    bool first = true;
+
+    for (int i = 0; i < queries.Count; i++)
+    {
+        first = true;
+        for (int j = 0; j < strings.Count; j++)
+        {
+            if (queries[i].Equals(strings[j]))
+            {
+                if (first)
+                {
+                    first = false;
+                    resultsQuerys.Add(1);
+                }
+
+                else
+                {
+                    resultsQuerys[i] += 1;
+                }
+            }
+
+         
+        }
+
+        if (first)
+        {
+            resultsQuerys.Add(0);
+        }
+    }
+
+    //Console.WriteLine(resultsQuerys[3]);
+    return resultsQuerys;
+}
+
+List<string> strings = new List<string> { "ab", "ab", "abc" };
+List<string> queries = new List<string> { "ab", "abc", "bc" };
+matchingStrings(strings, queries);
