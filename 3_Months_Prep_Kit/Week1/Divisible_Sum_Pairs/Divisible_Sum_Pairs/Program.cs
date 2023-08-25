1fb9f436b25a53e1bea12165697d48124ec93958
Togu <timeofgetup@gmail.com>
2023-08-24 20:54:07 -0500

    Divisible sum pairs solution

---
+++ 3_Months_Prep_Kit/Week1/Divisible_Sum_Pairs/Divisible_Sum_Pairs/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+
+
+static int divisibleSumPairs(int n, int k, List<int> ar)
+{
+
+    int pairsCount = 0;
+    List<int> alreadyUsed = new List<int> { -10 };
+
+
+    for (int i = 0; i < ar.Count; i++)
+    {
+        for (int j = 0; j < ar.Count; j++)
+        {
+            if (i < j)
+            {
+                int sum = ar[i] + ar[j];
+                if (sum % k == 0)
+                {
+                    for (int m = 0; m < alreadyUsed.Count; m++)
+                    {
+                        if (alreadyUsed[m] == ar[i])
+                        {
+                            break;
+                        }
+                    }
+
+                    alreadyUsed.Add(ar[i]);
+                    pairsCount++;
+
+
+                }
+            }
+        }
+    }
+
+    Console.WriteLine("Pares:" + pairsCount);
+    return pairsCount;
+}
+
+
+
+
+List<int> ar = new List<int> { 1, 3, 2, 6, 1, 2 };
+divisibleSumPairs(6, 3, ar);
