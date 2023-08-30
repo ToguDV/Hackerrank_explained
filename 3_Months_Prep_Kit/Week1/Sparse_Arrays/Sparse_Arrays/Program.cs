4653ff15855974e47290c5ce21b311ab40e64411
Togu <timeofgetup@gmail.com>
2023-08-30 00:05:30 -0500

    better solution

--- 3_Months_Prep_Kit/Week1/Sparse_Arrays/Sparse_Arrays/Program.cs
+++ 3_Months_Prep_Kit/Week1/Sparse_Arrays/Sparse_Arrays/Program.cs
@@ -2,37 +2,21 @@
 static List<int> matchingStrings(List<string> strings, List<string> queries)
 {
     List<int> resultsQuerys = new List<int>();
-    bool first = true;
+
 
     for (int i = 0; i < queries.Count; i++)
     {
-        first = true;
+        resultsQuerys.Add(0);
         for (int j = 0; j < strings.Count; j++)
         {
             if (queries[i].Equals(strings[j]))
             {
-                if (first)
-                {
-                    first = false;
-                    resultsQuerys.Add(1);
-                }
-
-                else
-                {
-                    resultsQuerys[i] += 1;
-                }
+                resultsQuerys[i] += 1;
             }
-
-         
-        }
-
-        if (first)
-        {
-            resultsQuerys.Add(0);
         }
     }
 
-    //Console.WriteLine(resultsQuerys[3]);
+    Console.WriteLine(resultsQuerys[0]);
     return resultsQuerys;
 }
 
