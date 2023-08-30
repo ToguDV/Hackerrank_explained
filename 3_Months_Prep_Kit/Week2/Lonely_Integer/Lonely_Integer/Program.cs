2694fdb12de4c4a345ef5f1165190745ef9eded5
Togu <timeofgetup@gmail.com>
2023-08-30 18:12:06 -0500

    Lonely integer solution

---
+++ 3_Months_Prep_Kit/Week2/Lonely_Integer/Lonely_Integer/Program.cs
@@ -0,0 +1,27 @@
+﻿static int lonelyinteger(List<int> a)
+{
+    bool unique;
+    int uniqueValue = 0;
+    for (int i = 0; i < a.Count; i++)
+    {
+        unique = true;
+        for (int j = 0; j < a.Count; j++)
+        {
+            if (a[i] == a[j] && j != i)
+            {
+                unique = false;
+                break;
+            }
+        }
+
+        if(unique)
+        {
+            uniqueValue = a[i];
+            
+        }
+    }
+    return uniqueValue;
+}
+
+List<int> a = new List<int> { 1, 2, 3, 4, 3, 2, 1 };
+Console.WriteLine(lonelyinteger(a));
