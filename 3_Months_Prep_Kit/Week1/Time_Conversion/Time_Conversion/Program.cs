81e8b711c364cd1f016082f121a8de9d25582675
Togu <timeofgetup@gmail.com>
2023-08-29 23:15:12 -0500

    Explanation

--- 3_Months_Prep_Kit/Week1/Time_Conversion/Time_Conversion/Program.cs
+++ 3_Months_Prep_Kit/Week1/Time_Conversion/Time_Conversion/Program.cs
@@ -1,18 +1,10 @@
 ﻿static string timeConversion(string s)
 {
-    string hours = "";
-    int hoursInt = 0;
-    string minutes = "";
-    string seconds = "";
-    string meridiem = "";
-
-    hours = s.Substring(0, 2);
-    minutes = s.Substring(3, 2);
-    seconds = s.Substring(6, 2);
-    meridiem = s.Substring(8, 2);
-
-    hoursInt = Int16.Parse(hours);
-
+    string hours = s.Substring(0, 2);
+    string minutes = s.Substring(3, 2);
+    string seconds = s.Substring(6, 2);
+    string meridiem = s.Substring(8, 2);
+    int hoursInt = Int16.Parse(hours);
 
     if(meridiem.Equals("PM") && hoursInt != 12)
     {
@@ -24,14 +16,13 @@
     {
         hours = "00";
     }
-
-
-
     return hours+":"+minutes+":"+seconds;
-
-
 }
 
-
-//Console.WriteLine(timeConversion("07:05:45PM"));
-//Console.WriteLine(timeConversion("12:45:54PM"));
+/* The formula to convert to 24 hours format when is "PM" : hours + 12 hours
+ * Example: 4:35 PM + 12 hours = 16:35
+ * 
+ * 
+ */
+Console.WriteLine(timeConversion("07:05:45PM"));
+Console.WriteLine(timeConversion("12:45:54PM"));
