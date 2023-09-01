3e2aefc31c24422a72a9756c4e8b57c856127b0e
Togu <timeofgetup@gmail.com>
2023-09-01 01:49:55 -0500

    Solution Flipping_Bits

---
+++ 3_Months_Prep_Kit/Week2/Flipping_Bits/Flipping_Bits/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Runtime.Intrinsics.Arm;
+
+static long FlippingBits(long n)
+{
+    uint y = (uint)n;
+    return ~y;
+}
+
+long originalValue = 1;
+long flippedValue = FlippingBits(originalValue);
+Console.WriteLine($"Original: {originalValue}, Flipped: {flippedValue}");
