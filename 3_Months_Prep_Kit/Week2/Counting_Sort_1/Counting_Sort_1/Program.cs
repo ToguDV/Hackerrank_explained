12cde558aa24920785c05ff1b103c8f5f33f816b
Togu <timeofgetup@gmail.com>
2023-09-02 00:16:07 -0500

    Counting Sort 1 Solution

---
+++ 3_Months_Prep_Kit/Week2/Counting_Sort_1/Counting_Sort_1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+static List<int> CountingSort(List<int> arr)
+{
+    List<int> count = new List<int>(new int[100]);
+
+    for (int i = 0; i < arr.Count(); i++)
+    {
+        count[arr[i]]++;
+    }
+
+    return count;
+}
+
