f3c98ae7e506704e0192779efdb849c5534a7255
Togu <timeofgetup@gmail.com>
2023-09-02 05:31:35 -0500

    Counting_Valleys solution

---
+++ 3_Months_Prep_Kit/Week2/Counting_Valleys/Counting_Valleys/Program.cs
@@ -0,0 +1,37 @@
+﻿static int countingValleys(int steps, string path)
+{
+    int level = 0;
+    int valleys = 0;
+    bool insideValley = false;
+
+    for (int i = 0; i < path.Length; i++)
+    {
+        char step = path[i];
+
+        if (step == 'U')
+        {
+            level++;
+        }
+        else if (step == 'D')
+        {
+            level--;
+        }
+
+        if (level < 0 && !insideValley)
+        {
+            insideValley = true;
+        }
+
+        if (level == 0 && insideValley)
+        {
+            valleys++;
+            insideValley = false;
+        }
+    }
+
+    return valleys;
+}
+
+string path = "DDUUDDDUDUUDUDDDUUDDUDDDUDDDUDUUDDUUDDDUDDDUDDDUUUDUDDDUDUDUDUUDDUDUDUDUDUUUUDDUDDUUDUUDUUDUUUUUUUUU";
+int result = countingValleys(10, path);
+Console.WriteLine(result);
