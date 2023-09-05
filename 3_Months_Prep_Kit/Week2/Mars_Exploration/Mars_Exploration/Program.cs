747aa9b18a9cf84ea94e0a4d718ecc93afb63f35
Togu <timeofgetup@gmail.com>
2023-09-04 21:09:40 -0500

    Mars_Exploration solution

---
+++ 3_Months_Prep_Kit/Week2/Mars_Exploration/Mars_Exploration/Program.cs
@@ -0,0 +1,25 @@
+﻿static int marsExploration(string s)
+{
+    int x = s.Count();
+    int count = 0;
+    for (int i = 0; i < x; i += 3)
+    {
+        int j = i;
+        if (s[j] != 'S')
+        {
+            count += 1;
+        }
+        if (s[j+1] != 'O')
+        {
+            count += 1;
+        }
+        if (s[j + 2] != 'S')
+        {
+            count += 1;
+        }
+    }
+    Console.WriteLine(count);
+    return count;
+}
+
+marsExploration("SOSOSOSOSDSDSKWOSDOSDOASDOASDFAFJDFDOSOSOWNSOSOSNDSKDDOSOSOSJDSDSOOSOSDSDOSASSOASDSAOSOSODSDSOASDWS");
