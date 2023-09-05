d39d35b8ea8292293bb18cbf4308d4eadbc30992
Togu <timeofgetup@gmail.com>
2023-09-04 20:17:22 -0500

    Pangrams solution

---
+++ 3_Months_Prep_Kit/Week2/Pangrams/Pangrams/Program.cs
@@ -0,0 +1,17 @@
+﻿static string pangrams(string s)
+{
+    HashSet<char> letters = new HashSet<char>();
+
+    foreach (char c in s.ToLower())
+    {
+        if (Char.IsLetter(c))
+        {
+            letters.Add(c);
+        }
+    }
+
+    return letters.Count == 26 ? "pangram" : "not pangram";
+}
+
+
+Console.WriteLine(pangrams("We promptly judged antique ivory buckles for the prize"));
